95c338ac0993583d7ba67fc81c6c4a17594aaeee
Alexander Chesser <alex.chesser@zensurance.com>
2021-09-10 20:29:33 -0400

    prepare the object properly

--- src/controllers/CommandController.ts
+++ src/controllers/CommandController.ts
@@ -1,7 +1,7 @@
 import { ICommand } from "../interface/ICommand";
 
 import * as mongoose from "mongoose";
-import { Command } from "../model/Command";
+import { Command, ICommandDocument } from "../model/Command";
 
 export async function add(req: ICommand): Promise<string> {
   let message: string = "you should not be getting this message :'(";
@@ -11,7 +11,8 @@ export async function add(req: ICommand): Promise<string> {
     user_id: req.user_id,
   };
   await mongoose.connect(process.env.MONGODB_URI || "");
-  const command = new Command(req);
+  const command = new Command(req as ICommandDocument).toObject();
+  delete command["_id"];
   await Command.updateOne(key, command, { upsert: true }).exec();
   message = "You have been added to the channel";
   return message;
