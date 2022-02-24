f1d4f4abc4c48ef165a5d407ae7e7c873979e133
Alexander Chesser <alex.chesser@zensurance.com>
2022-02-23 23:39:27 -0500

    I legitimately have no idea what I'm doing 😭

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import { logger } from './utils/logger';
 
 import * as APP_SETTINGS from './utils/app_settings';
-import { APIGatewayEvent, Context } from "aws-lambda";
-import { App, ExpressReceiver, ReceiverEvent } from "@slack/bolt";
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import { App, ExpressReceiver, ReceiverEvent } from '@slack/bolt';
 
-import * as FikaCommandController from "./controllers/FikaCommandController";
-
+import * as FikaCommandController from './controllers/FikaCommandController';
 
 /**
  * Initialize Slack Bot communication with bot token and signing secret
  */
 const expressReceiver = new ExpressReceiver({
 	signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
-	processBeforeResponse: true,
+	processBeforeResponse: true
 });
 
 const app = new App({
@@ -24,41 +23,57 @@ const app = new App({
 /**
  * Configure Slack Bot Commands
  */
-app.command(
-	APP_SETTINGS.config.FIKA_SLASH_COMMAND_USERMANAGEMENT,
-	async ({ body, ack, respond, client }) => {
-		return await FikaCommandController.processCommand(
-			body,
-			ack,
-			respond,
-			client
-		);
-	}
-);
+app.command(APP_SETTINGS.config.FIKA_SLASH_COMMAND_USERMANAGEMENT, async ({ body, ack, respond, client }) => {
+	return await FikaCommandController.processCommand(body, ack, respond, client);
+});
 
 /**
  * Helper function to parse request body.  Depending if the request is coming from slackbot command, or coming from slackbot event, the body is different
  */
 function parseRequestBody(stringBody: string | null, contentType: string | undefined) {
 	try {
-		let inputStringBody: string = stringBody ?? "";
+		let inputStringBody: string = stringBody ?? '';
 		// depending on incoming request, body can be formated as urlencoded form elements or just decoded string
 		// determine appropriate format to decode body content
-		if (contentType && contentType === "application/x-www-form-urlencoded") {
+		if (contentType && contentType === 'application/x-www-form-urlencoded') {
 			let result: any = {};
-			var keyValuePairs = inputStringBody.split("&");
+			var keyValuePairs = inputStringBody.split('&');
 			keyValuePairs.forEach(function (pair: string): void {
-				let individualKeyValuePair: string[] = pair.split("=");
-				result[individualKeyValuePair[0]] = decodeURIComponent((individualKeyValuePair[1] || "").replace("+", " "));
+				let individualKeyValuePair: string[] = pair.split('=');
+				result[individualKeyValuePair[0]] = decodeURIComponent((individualKeyValuePair[1] || '').replace('+', ' '));
 			});
 			return JSON.parse(JSON.stringify(result));
-
 		} else {
 			return JSON.parse(inputStringBody);
 		}
 	} catch {
 		return undefined;
 	}
+}
+
+async function Oauth(event: APIGatewayEvent) {
+	const code = !!event.queryStringParameters && !!event.queryStringParameters.code ? event.queryStringParameters.code : null;
+	if (!code) {
+		// access denied
+		logger.log('Access denied');
+		return {
+			statusCode: 200,
+			body: 'access-denied'
+		};
+	}
+	const data = {
+		form: {
+			client_id: process.env.SLACK_CLIENT_ID,
+			client_secret: process.env.SLACK_SIGNING_SECRET,
+			code: code
+		}
+	};
+	const response = await fetch('https://slack.com/api/oauth.access', {
+		method: 'POST',
+		body: JSON.stringify(data)
+	});
+	logger.info(JSON.stringify(response));
+	return response;
 }
 
 /**
@@ -72,14 +87,17 @@ function parseRequestBody(stringBody: string | null, contentType: string | undef
  */
 export async function handler(event: APIGatewayEvent, context: Context) {
 	try {
-		logger.info(`event: ${JSON.stringify(event)}, context: ${JSON.stringify(context)}`)
+		if (event.httpMethod === 'GET') {
+			return await Oauth(event);
+		}
+		logger.info(`event: ${JSON.stringify(event)}, context: ${JSON.stringify(context)}`);
 		// verify incoming request is valid
-		const payload = parseRequestBody(event.body, event.headers["content-type"]);
-		logger.info("payload:", payload);
-		if (payload && payload.type && payload.type === "url_verification") {
+		const payload = parseRequestBody(event.body, event.headers['content-type']);
+		logger.info('payload:', payload);
+		if (payload && payload.type && payload.type === 'url_verification') {
 			return {
 				statusCode: 200,
-				body: payload.challenge,
+				body: payload.challenge
 			};
 		}
 
@@ -91,21 +109,21 @@ export async function handler(event: APIGatewayEvent, context: Context) {
 					resolve();
 					return {
 						statusCode: 200,
-						body: response ?? "",
+						body: response ?? ''
 					};
 				});
-			},
+			}
 		};
 		await app.processEvent(slackEvent);
 		return {
 			statusCode: 200,
-			body: "",
+			body: ''
 		};
 	} catch (e) {
-		logger.error("There was an unexpected error: ", e);
+		logger.error('There was an unexpected error: ', e);
 		return {
 			statusCode: 500,
-			body: "There was an unexpected error:" + e,
+			body: 'There was an unexpected error:' + e
 		};
 	}
 }
